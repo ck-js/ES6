86cf7affafcfa51ce0860a1a0803c529ec99b941
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-20 16:50:38 +0700

    Add const variables of arrays and objects to show that you can update const variables but only through methods. Learn about variable hoisting where we assign value to a variable without declaring it which can only be done for var variables.

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var surname = "Lebron";
+var surname = "Lebronie";
+console.log(surname)
+
+const color = "Green";
+console.log(color);
+
+let soup = "Chicken";
+  soup = "Miso";
+ console.log(soup);
+// cant update const variables but able to use array methods.
+ const newPresent = ["Jeans"];
+ const wardrobe = ["Shirts", "Socks"];
+ wardrobe.push(newPresent);
+ console.log(wardrobe);
+
+const myClothes = {
+    tShirts: 2,
+    slacks: 1,
+}
+myClothes.slacks++;
+console.log(myClothes)
+
+dob = 2002;
+var dob = 2001;
+console.log(dob);
