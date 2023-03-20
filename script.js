99eaae75246aaecb68cd21ccdbdb10a3006157de
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-20 17:38:20 +0700

    Add code examples of global, local and block scope for various variable types and tried accessing different variables in different scopes and seeing which ones produce an error. For example a cost variable declared in local scope cannot be accessed in the global scope

--- script.js
+++ script.js
@@ -25,3 +25,49 @@ console.log(myClothes)
 dob = 2002;
 var dob = 2001;
 console.log(dob);
+
+// the const variable below cannot be accessed in global scope
+function displayMessage() {
+    const variable = "This variable cannot be accessed outside of this scope";
+    console.log(variable);
+}
+ displayMessage();
+
+ // Whereas the let variable can be accessed by local scope in global scope source
+let cityToVisit = ["Tokyo", "Paris"];
+function showTravel() {
+    console.log("Next year, travel to " + cityToVisit);
+}
+showTravel();
+console.log(cityToVisit);
+
+if (10 === 10) {
+    console.log("This space is called block scope");
+}
+
+const drivingAge = 18;
+let canDrive = false;
+if (drivingAge >= 18) {
+    let canDrive = true;
+    console.log(canDrive);
+}
+console.log(canDrive);
+
+for (let index = 0; index < 4; index++) {
+    console.log(index);
+}
+
+//if a var variabkle is declared inside a block scope you can still access in global scope
+if ( 2 === 2) {
+    var result = "Numbers match";
+}
+console.log(result)
+
+// whereas you cannot access a const variable declared in local scope in global scope
+function fixHouse() {
+    const fix = "Fix mirror";
+}
+console.log(fix);
+
+
+
